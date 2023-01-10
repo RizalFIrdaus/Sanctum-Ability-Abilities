e6f4e8ff4ab5133c37e8d777d4870d0353f6d311
RizalFIrdaus <rizal.firdaus117@gmail.com>
2023-01-10 07:56:55 +0700

    Api method Register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $request->validate([
+            'role' => 'required',
+            'name' => 'required|min:3|max:12',
+            'email' => 'required|email',
+            'password' => [
+                'required',
+                Password::min(6)
+                    ->letters()
+                    ->mixedCase()
+                    ->numbers()
+                    ->symbols()
+            ]
+        ]);
+
+        $user = User::create([
+            'role' => $request->role,
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Berhasil Register',
+            'data' => $user
+        ]);
+    }
 }
